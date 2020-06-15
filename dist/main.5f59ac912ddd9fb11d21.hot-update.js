f3b0e527be3fa69ba2c3512855ee971a0dfe09d5
Abraham Mayowa Oluwasina <mayowa@Abrahams-iMac.local>
2020-06-15 04:36:21 -0700

    webpackSetup done

---
+++ dist/main.5f59ac912ddd9fb11d21.hot-update.js
@@ -0,0 +1,15 @@
+exports.id = "main";
+exports.modules = {
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module parse failed: Unterminated regular expression (40:9)\\nFile was processed with these loaders:\\n * ./node_modules/ts-loader/index.js\\nYou may need an additional loader to handle the result of these loaders.\\n| app.use(body_parser_1.default.json());\\n| app.use(body_parser_1.default.urlencoded({ extended: true }));\\n> app.get(/’, function (req, res){, res.send(Hello, World));\\n| var server = app.listen(PORT, function () {\\n|     console.log(\\\"Listening on port \\\" + PORT);\");\n\n//# sourceURL=webpack:///./src/index.ts?");
+
+/***/ })
+
+};
--- src/index.ts
+++ src/index.ts
@@ -19,8 +19,33 @@ app.use(cors());
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-
+app.get('/', function (req, res){
+    //res.send('Hello World!'
+})
 
 const server = app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
+
+  type ModuleId = string | number;
+
+  interface WebpackHotModule {
+    hot?: {
+      data: any;
+      accept(
+        dependencies: string[],
+        callback?: (updatedDependencies: ModuleId[]) => void,
+      ): void;
+      accept(dependency: string, callback?: () => void): void;
+      accept(errHandler?: (err: Error) => void): void;
+      dispose(callback: (data: any) => void): void;
+    };
+  }
+  
+  declare const module: WebpackHotModule;
+  
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => server.close());
+  }
+  
